7b66e96902fa9e9877c1e721cf31f6f4c1b5ea38
Stephanie <88047028+smackin@users.noreply.github.com>
2022-08-23 13:04:41 -0400

    Add files via upload
    part 1 of exercises

---
+++ part-1/App.js
@@ -0,0 +1,18 @@
+// Part 1
+// For this assignment you will be creating three components:
+
+// FirstComponent
+// renders an h1 with the text “My very first component”.
+// NamedComponent
+// renders a p that should accept a property of “name” and display text “My name is name”.
+// App
+// renders a div with instances of the other two components.
+
+function App() {
+    return (
+      <div>
+        <FirstComponent />
+        <NamedComponent name="Bob" />
+      </div>
+    );
+  }
