1f9d8851da5616e560ac0216e1b9715e952486f3
Stephanie <88047028+smackin@users.noreply.github.com>
2022-08-23 13:09:48 -0400

    Add files via upload

--- part-1/App.js
+++ part-1/App.js
@@ -12,7 +12,7 @@ function App() {
     return (
       <div>
         <FirstComponent />
-        <NamedComponent name="Bob" />
+        <NamedComponent name="Stephanie"/>
       </div>
     );
   }
