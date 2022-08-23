848c2e33636ea4816bb850a1b7606165a80b516f
Stephanie <88047028+smackin@users.noreply.github.com>
2022-08-23 13:13:50 -0400

    Add files via upload
    updated index.html

--- part-2/App.js
+++ part-2/App.js
@@ -1,3 +1,8 @@
+// Part 2
+// Define a Tweet component which takes as props the username of the user who wrote the tweet, the name of the user who wrote the tweet, the date of the tweet, and the message being tweeted.
+
+// Create an App component that renders at least three tweets.
+
 function App() {
     return (
       <div>
@@ -5,13 +10,13 @@ function App() {
           name="Matt Lane"
           username="mmmaaatttttt"
           date={new Date().toDateString()}
-          message="This app will disrupt everything!!"
+          message="This app is the best!!"
         />
         <Tweet
           name="Elie Schoppik"
           username="eschoppik"
           date={new Date().toDateString()}
-          message="#Ilovehashtags"
+          message="I love my pupper! "
         />
         <Tweet
           name="Tim Garcia"
